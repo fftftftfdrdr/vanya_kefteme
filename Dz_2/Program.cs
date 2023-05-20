67f6189c152795b5411bdee150f136c0eebfbd7c
Ivan Slukin <provaider010@gmail.com>
2023-05-20 13:16:32 +1000

    prosto dz

---
+++ Dz_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b & a > c)
+{
+    Console.WriteLine("максимальное число - ");
+Console.Write(a);
+}
+else if (b > c)
+{
+Console.WriteLine("максимальное число - ");
+Console.Write(b);
+}
+else 
+{
+Console.WriteLine("Максимальное число - ");
+Console.Write(c);
+}
+
